5e2dbc0a0ce16ef546c5d4c5b3e1c2ee9ec5262b
Takatoshi-Miura <takatoshi052031@gmail.com>
2020-06-04 16:37:11 +0900

    FirebaseのRealtime DatabaseにTodoの情報を保存するように改良。

--- 002-MyTodoList/ViewController.swift
+++ 002-MyTodoList/ViewController.swift
@@ -7,15 +7,20 @@
 //
 //  ＜概要＞
 //  Todoリストを作成するプログラム。
-//  「＋」ボタンをタップでTodoを入力するダイアログが表示され、OKボタンをタップでリストに追加される。
-//  リストの内容は todoList という名前の配列に格納する。
-//  リストデータの永続化はUserDefaultsを使用し、"todoList"という名前のキーを指定する。
-//
-//  セルに表示されるチェックマークの有無によって達成済みと未達成のTodoを区別する。
-//  MyTodoクラスを用意し、Todoの達成状況を格納する変数todoDoneを定義している。
-//  MyTodoクラスのデータをUserDefaultsで保存するため、シリアライズ処理を行っている。
-//
+//  「＋」ボタンをタップでTodoを入力するダイアログが表示され、内容を入力後OKボタンタップでリストに追加される。
+//  セルをタップする毎にチェックマークの有無が切り替わることで、達成度がわかる。
 //  セルを左にスワイプすることでTodoを削除できる。
+//  リストのデータはFirebaseのRealtime Databeseに保存される。
+//
+//  Todoを作成する毎にMyTodoクラスのオブジェクトが生成される。
+//  Todoにはそれぞれ固有のIDを設定することによって区別する。
+//  固有IDは一度設定されたら削除されるまで変更されることはなく、Todo作成毎に1から昇順に割り当てられる。
+//  アプリ起動時にデータベースからデータを読み取るが、このとき固有IDの最大値を取得することで、IDの重複を防止する。
+//
+//  ＜課題＞
+//  Todoが削除されることで固有IDの空き番号が出てくる。
+//  新しいTodoを追加する際に、その空き番号を優先的に割り当てるようにしたい。
+//  現状では、Int型の範囲を超える個数のTodoは扱うことができない。
 //
 
 import UIKit
@@ -25,19 +30,24 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         //保存しているToDoの読み込み処理
-        let userDefaults = UserDefaults.standard
-        if let storedTodoList = userDefaults.object(forKey:"todoList") as? Data{
-            do {
-                if let unarchiveTodoList = try NSKeyedUnarchiver.unarchivedObject(
-                    ofClasses:[NSArray.self,MyTodo.self],
-                from:storedTodoList) as? [MyTodo]{
-                    todoList.append(contentsOf:unarchiveTodoList)
+        for todoID in 0...100 {
+            Database.database().reference().child("user/\(todoID)").observeSingleEvent(of:.value, with:{(snapshot) in
+                if let data = snapshot.value as? [String:AnyObject]{
+                    //データベースのデータを元にリストを作成する
+                    //データベースに保存されている固有IDを反映する
+                    let myTodo = MyTodo(todoID)
+                    //Todoの内容と達成度をデータベースから読み取る
+                    myTodo.todoTitle = data["myTodo"] as? String
+                    myTodo.todoDone  = data["todoDone"] as! NSObject as! Bool
+                    //リストに追加し、テーブルに反映
+                    self.todoList.insert(myTodo,at:0)
+                    self.tableView.insertRows(at: [IndexPath(row:0,section:0)],with: UITableView.RowAnimation.right)
                 }
-            } catch {
-                //エラー処理無し
-            }
+            }, withCancel: nil)
         }
+        
     }
     
     @IBOutlet weak var tableView: UITableView!
@@ -45,13 +55,10 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     //ToDoを格納した配列
     var todoList = [MyTodo]()
     
-    
     //「＋」ボタンをタップした時の処理
     @IBAction func tapAddButton(_ sender: Any) {
         //アラートダイアログを生成
-        let alertController = UIAlertController(title:"TODO追加",
-                                                message:"ToDoを入力してください",
-                                                preferredStyle:UIAlertController.Style.alert)
+        let alertController = UIAlertController(title:"TODO追加",message:"ToDoを入力してください",preferredStyle:UIAlertController.Style.alert)
         //テキストエリアを追加
         alertController.addTextField(configurationHandler:nil)
         
@@ -65,28 +72,20 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
                 myTodo.todoTitle = textField.text!
                 self.todoList.insert(myTodo,at:0)
                 //テーブルに行が追加されたことをテーブルに通知
-                self.tableView.insertRows(at: [IndexPath(row:0,section:0)],
-                                          with: UITableView.RowAnimation.right)
-                //ToDoの保存処理
-                let userDefaults = UserDefaults.standard
-                //Data型にシリアライズする
-                do {
-                let data = try NSKeyedArchiver.archivedData(
-                    withRootObject:self.todoList,requiringSecureCoding:true)
-                    userDefaults.set(data,forKey:"todoList")
-                    userDefaults.synchronize()
-                } catch {
-                    //エラー処理なし
-                }
+                self.tableView.insertRows(at: [IndexPath(row:0,section:0)],with: UITableView.RowAnimation.right)
+                //Todoの内容と達成度をデータベースに保存
+                let databaseReference:DatabaseReference! = Database.database().reference()
+                let titleData = ["\(myTodo.getTodoID())/myTodo":myTodo.todoTitle]
+                let doneData  = ["\(myTodo.getTodoID())/todoDone":myTodo.todoDone]
+                databaseReference.child("user").updateChildValues(titleData as [AnyHashable : Any])
+                databaseReference.child("user").updateChildValues(doneData)
             }
         }
         //OKボタンを追加
         alertController.addAction(okAction)
         
         //CANCELボタンを宣言
-        let cancelButton = UIAlertAction(title:"CANCEL",
-                                         style:UIAlertAction.Style.cancel,
-                                         handler:nil)
+        let cancelButton = UIAlertAction(title:"CANCEL",style:UIAlertAction.Style.cancel,handler:nil)
         //CANCELボタンを追加
         alertController.addAction(cancelButton)
         
@@ -121,28 +120,19 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     //セルをタップしたときの処理
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Todoが達成済みなら未達成に、未達成なら達成済みに変更
         let myTodo = todoList[indexPath.row]
         if myTodo.todoDone {
-            //完了済みの場合は未完了に変更
             myTodo.todoDone = false
         } else {
-            //未完の場合は完了済みに変更
             myTodo.todoDone = true
         }
         //セルの状態を変更
-        tableView.reloadRows(at:[indexPath],
-                             with:UITableView.RowAnimation.fade)
-        //データ保存。Data型にシリアライズする
-        do {
-            let data:Data = try NSKeyedArchiver.archivedData(
-                withRootObject:todoList,requiringSecureCoding:true)
-            //UserDefaultsに保存
-            let userDefaults = UserDefaults.standard
-            userDefaults.set(data,forKey:"todoList")
-            userDefaults.synchronize()
-        } catch {
-            print("デバック:データの保存に失敗しました")
-        }
+        tableView.reloadRows(at:[indexPath],with:UITableView.RowAnimation.fade)
+        //Todoの達成度をデータベースに保存
+        let databaseReference:DatabaseReference! = Database.database().reference()
+        let doneData = ["\(myTodo.getTodoID())/todoDone":myTodo.todoDone]
+        databaseReference.child("user").updateChildValues(doneData)
     }
     
     
@@ -150,51 +140,51 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         //削除処理かどうか
         if editingStyle == UITableViewCell.EditingStyle.delete {
-            print("デバック:削除処理が行われました")
+            //データベースのデータを削除
+            let databaseReference:DatabaseReference! = Database.database().reference()
+            databaseReference.child("user/\(todoList[indexPath.row].getTodoID())").removeValue()
             //ToDoリストから削除
             todoList.remove(at:indexPath.row)
             //セルを削除
             tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
-            //データ保存。Data型にシリアライズする
-            do {
-                let data:Data = try NSKeyedArchiver.archivedData(
-                    withRootObject:todoList,requiringSecureCoding:true)
-                //UserDefaultsに保存
-                let userDefaults = UserDefaults.standard
-                userDefaults.set(data,forKey:"todoList")
-                userDefaults.synchronize()
-            } catch {
-                //エラー処理なし
-            }
         }
     }
     
 }
 
 
-//独自クラスをシリアライズする際には、NSObjectを継承し
-//NSSecureCodingプロトコルに準拠する必要がある
-class MyTodo:NSObject,NSSecureCoding{
+//固有IDを設定することによってTodoを区別する
+class MyTodo {
+    
     static var supportsSecureCoding:Bool{
         return true
     }
 
+    static var todoCount:Int = 0 //登録したTodoの総合計
+    private let todoID:Int       //Todoの固有ID。一度設定したらTodoが削除されるまで不変なため定数で宣言。
     var todoTitle:String?        //ToDoのタイトル
-    var todoDone :Bool = false   //ToDoを完了したかどうかを表すフラグ
+    var todoDone:Bool = false    //ToDoを完了したかどうかを表すフラグ
     
-    override init(){
+    //Todo作成時の処理。固有IDの割り当て。
+    init(){
+        MyTodo.todoCount += 1
+        self.todoID = MyTodo.todoCount
+    }
+    
+    //データベースからの読み取り用
+    init(_ todoID:Int){
+        self.todoID = todoID
+        //todoIDの最大値をtodoCountに設定することで、todoIDの重複を防止
+        if todoID > MyTodo.todoCount {
+            MyTodo.todoCount = todoID
+        }
+    }
+    
+    //固有IDのゲッター
+    public func getTodoID() -> Int {
+        return self.todoID
     }
 
-    //NSCodingプロトコルに宣言されているデシリアライズ処理。デコード処理とも呼ばれる
-    required init?(coder aDecoder:NSCoder){
-        todoTitle = aDecoder.decodeObject(forKey:"todoTitle") as? String
-        todoDone  = aDecoder.decodeBool(forKey:"todoDone")
-    }
-    //NSCodingプロトコルに宣言されているシリアライズ処理。エンコード処理とも呼ばれる
-    func encode(with aCoder:NSCoder){
-        aCoder.encode(todoTitle,forKey:"todoTitle")
-        aCoder.encode(todoDone ,forKey:"todoDone")
-    }
 }
 
 
